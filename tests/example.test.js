7f13713343ee9f49494dabe2fab34bec3fa350fc
THORUNG B. <thorung.b@gmail.com>
2024-01-22 19:36:44 +0700

    edit test script

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,8 +2,8 @@ import { shallow } from 'enzyme';
 import React from 'react';
 import App from '../src/app.js';
 describe('With Enzyme', () => {
-  it('App shows "Hi CICD"', () => {
+  it('App shows "Hello Word"', () => {
    const app = shallow(<App />);
-   expect(app.find('p').text()).toEqual('Hi CICD');
+   expect(app.find('p').text()).toEqual('Hi MY CICD');
  });
 });
